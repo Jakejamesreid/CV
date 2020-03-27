7ddac6de7a64f65f1b20265a89739488311044d6
jakejamesreid <jakejamesreid@gmail.com>
2020-03-27 15:51:59 +0000

    Added Markers to Google Maps

---
+++ assets/js/maps.js
@@ -0,0 +1,29 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        {lat: 40.785091, lng: -73.968285},
+        {lat: 41.084045, lng: -73.968285},
+        {lat: 40.754932, lng: -73.968285}
+    ];
+
+    // locations.map is a Javascript function that works similar to a forEach function but instead it returns an array, unrelated to Google Maps
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
