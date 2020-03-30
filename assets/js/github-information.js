43b6b525d78ace622428a333a97eca9e83c0d271
jakejamesreid <jakejamesreid@gmail.com>
2020-03-30 20:10:13 +0100

    Form Only submits on enter button to limit API calls

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -79,3 +79,14 @@ function fetchGitHubInformation(event) {
 }
 
 $(document).ready(fetchGitHubInformation);
+
+// Execute a function when the user releases a key on the keyboard
+addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    fetchGitHubInformation();
+  }
+});
